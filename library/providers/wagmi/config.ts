c75fc7d7562fb05252bddc66ec094ec452e7c5a0
kelvinpraises <kelvinpraises@gmail.com>
2025-02-23 20:54:28 +0100

    chore: Update app name in Wagmi configuration

--- library/providers/wagmi/config.ts
+++ library/providers/wagmi/config.ts
@@ -6,7 +6,7 @@ const isDev = process.env.NODE_ENV === 'development';
 
 export const config = createConfig(
   getDefaultConfig({
-    appName: "Vidrune",
+    appName: "SonicStand",
     walletConnectProjectId:
       process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
     chains: isDev ? [anvil] : [sepolia],
