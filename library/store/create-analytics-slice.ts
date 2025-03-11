5d611f6ab48479cba2d803831b2211f610099c46
kelvinpraises <kelvinpraises@gmail.com>
2025-03-11 09:20:15 +0100

    refactor: Update analytics slice to use completedIndexes instead of completedCaptions

--- library/store/create-analytics-slice.ts
+++ library/store/create-analytics-slice.ts
@@ -3,19 +3,19 @@ import { persist, createJSONStorage } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
 type AnalyticsState = {
-  completedCaptions: number;
+  completedIndexes: number;
   scenesProcessed: number;
   updateStats: (scenesCount: number) => void;
 };
 
 export default persist(
   immer<AnalyticsState>((set) => ({
-    completedCaptions: 0,
+    completedIndexes: 0,
     scenesProcessed: 0,
 
     updateStats: (scenesCount: number) =>
       set((state) => {
-        state.completedCaptions += 1; // Increment caption count by 1
+        state.completedIndexes += 1; // Increment caption count by 1
         state.scenesProcessed += scenesCount; // Add the number of scenes processed
       }),
   })),
