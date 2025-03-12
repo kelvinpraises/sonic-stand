9386547a330b6a8292b143408fa2fe27422ba3a0
kelvinpraises <kelvinpraises@gmail.com>
2025-03-12 10:02:21 +0100

    chore: Update Wagmi config to use Sonic Blaze Testnet RPC

--- library/providers/wagmi/config.ts
+++ library/providers/wagmi/config.ts
@@ -1,19 +1,19 @@
 import { getDefaultConfig } from "connectkit";
 import { createConfig, http } from "wagmi";
-import { anvil, sepolia } from "wagmi/chains";
+import { anvil, sonicBlazeTestnet } from "wagmi/chains";
 
-const isDev = process.env.NODE_ENV === 'development';
+const isDev = process.env.NODE_ENV === "development";
 
 export const config = createConfig(
   getDefaultConfig({
     appName: "SonicStand",
     walletConnectProjectId:
       process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
-    chains: isDev ? [anvil] : [sepolia],
+    chains: isDev ? [anvil] : [sonicBlazeTestnet],
     multiInjectedProviderDiscovery: true,
     transports: {
       [anvil.id]: http(),
-      [sepolia.id]: http("https://eth-sepolia.g.alchemy.com/v2/vEVKURyIJBdxxahH6eQJOSpjtIe3kA6-"),
+      [sonicBlazeTestnet.id]: http("https://sonic-blaze-rpc.publicnode.com"),
     },
   })
 );
